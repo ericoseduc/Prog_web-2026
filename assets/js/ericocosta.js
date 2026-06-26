6abd95e7a42ff162c84708c62ae9073d54ac28de
ericovinicosta <elcosta@gmail.com>
2026-06-26 00:18:41 -0300

    5c ericocosta

--- assets/js/ericocosta.js
+++ assets/js/ericocosta.js
@@ -4,7 +4,10 @@ const idadeFora = document.querySelector('#idade');
 const calculaIdade = (dn) => {
     let dtNascimento = new Date(dn.textContent.split(': ')[1].split('/').reverse().join('-'));
     let idade = new Date().getFullYear() - dtNascimento.getFullYear();
-    if(dn.textContent.split(': ')[1].split('/').reverse().join('-') > new Date().toISOString().split('T')[0]) {
+    if(dtNascimento.getMonth() > new Date().getMonth()){
+        idade--;
+    }
+    if(dtNascimento.getMonth() === new Date().getMonth() && dtNascimento.getDate() > new Date().getDate()){
         idade--;
     }
     return idade;
