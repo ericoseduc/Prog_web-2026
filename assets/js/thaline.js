36605478deecc6c6f2be7e8f2a4d288f828f41e1
tha-lin1 <thaline.cosme@aluno.edu.pi.gov.br>
2026-07-03 10:53:55 -0300

    4c thaline

--- assets/js/thaline.js
+++ assets/js/thaline.js
@@ -1,1 +1,11 @@
-const imgFoto = document.querySelector("#fotoimg")
+const dataNascimento =document.querySelector('dataNascimento');
+const idadeFora =document.querySelector('idadeFora');
+
+const calcularIdade = (dn) => {
+    let dtNascimento = new Date(dn.textContent.split(': ')[1].split('/').reverse().join('-'));
+    let idade = new Date ().getFullYear() - dtnascimento.getFullYear();
+    if dtNascimento.getMonth() > new Date().getMonth() || (dtNascimento.getMonth() === new Date().getMonth() && dtNascimento.getDate() > new Date().getDate()) {
+        idade--;
+    }
+    return idade;
+}
